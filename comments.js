8c577536700a77fd8434bf11990a969a39a17cdf
Deekshita D Nair <54467963+DEEKSHI12345@users.noreply.github.com>
2024-05-12 15:09:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,59 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var comments = [];
-//create server
-http.createServer(function(req, res){
-    //parse url
-    var urlObj = url.parse(req.url, true);
-    var pathName = urlObj.pathname;
-    if(pathName == '/'){
-        //read file
-        fs.readFile('./index.html', function(err, data){
-            if(err){
-                console.log(err);
-            }else{
-                res.writeHead(200, {'Content-Type':'text/html'});
-                res.end(data);
-            }
-        });
-    }else if(pathName == '/submit'){
-        var comment = urlObj.query;
-        comment.data = new Date();
-        comments.push(comment);
-        res.end(JSON.stringify(comments));
-    }else if(pathName == '/get'){
-        res.end(JSON.stringify(comments));
-    }else{
-        fs.readFile('.' + pathName, function(err, data){
-            if(err){
-                console.log(err);
-            }else{
-                res.end(data);
-            }
-        });
+const express = require('express');
+const app = express();
+app.use(express.json());
+//import data
+const comments = require('./data/comments');
+//import functions
+const { getNewId, getNewDate } = require('./utils');
+// Path: /comments
+//GET method
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+//POST method
+app.post('/comments', (req, res) => {
+    const { body } = req;
+    if (!body.text) {
+        return res.status(400).json({ msg: 'Text is required' });
     }
-}).listen(3000, function(){
-    console.log('Server running at http://
+    const newComment = {
+        id: getNewId(comments),
+        text: body.text,
+        date: getNewDate(),
+    };
+    comments.push(newComment);
+    res.json(newComment);
+});
+//GET method
+app.get('/comments/:id', (req, res) => {
+    const comment = comments.find(comment => comment.id === parseInt(req.params.id));
+    if (!comment) {
+        return res.status(404).json({ msg: `Comment with id ${req.params.id} not found` });
+    }
+    res.json(comment);
+});
+//PUT method
+app.put('/comments/:id', (req, res) => {
+    const { body } = req;
+    if (!body.text) {
+        return res.status(400).json({ msg: 'Text is required' });
+    }
+    const comment = comments.find(comment => comment.id === parseInt(req.params.id));
+    if (!comment) {
+        return res.status(404).json({ msg: `Comment with id ${req.params.id} not found` });
+    }
+    comment.text = body.text;
+    res.json(comment);
+});
+//DELETE method
+app.delete('/comments/:id', (req, res) => {
+    const index = comments.findIndex(comment => comment.id === parseInt(req.params.id));
+    if (index === -1) {
+        return res.status(404).json({ msg: `Comment with id ${req.params.id} not found` });
+    }
+    comments.splice(index, 1);
+    res.json({ msg: `Comment with id ${req.params.id} deleted` });
+});
+// Path: /comments/:id
+module.exports = app;
